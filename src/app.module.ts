4e13727417f0af16a8cdc6cf3eac4b61f6711be7
hardzal <suryadijogja@gmail.com>
2025-06-09 09:08:25 +0700

    common modules and prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { CommonService } from './prisma/common/common.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommonModule],
+  controllers: [],
+  providers: [CommonService],
 })
 export class AppModule {}
